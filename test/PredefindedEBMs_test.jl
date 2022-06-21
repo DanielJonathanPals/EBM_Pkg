ef7ac5ed6c31688565c17bb34c4b5777f787cfc3
DanielJonathanPals <104452598+DanielJonathanPals@users.noreply.github.com>
2022-06-21 12:10:00 +0200

    Add new test

--- test/PredefindedEBMs_test.jl
+++ test/PredefindedEBMs_test.jl
@@ -13,3 +13,14 @@ t = (0.,rand()*2)
 prob = ODEProblem(zero_d_ebm, u0, t,p)
 sol = solve(prob)
 @test all(sol .≈ solve(SimplestModel(u0,p,t)))
+
+
+function fp(p,ε=1)
+    α, S₀, σ = p
+    return ((1-α)*S₀/(4*σ*ε))^(1/4)
+end
+
+@test abs(solve((SimplestModel(u0,p,(0.,1000.))))[end] - fp(p)) < 0.1
+
+
+
